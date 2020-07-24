7b7793f377ce9a4fa9301eec59dd090bb3bd9468
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-24 14:59:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,8 @@ require_relative "../config/environment.rb"
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn] 
+
   attr_accessor :name, :grade
   
   attr_reader :id
@@ -52,3 +52,4 @@ class Student
   end
 
 end
+
