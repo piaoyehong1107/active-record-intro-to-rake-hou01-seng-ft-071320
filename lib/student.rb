7b691a1319835a81a003f74979edd603b2d9cd4a
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-24 14:39:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn] 
-  task :environment do
+task :environment do
   require_relative './config/environment'
 end
   
